3e9d41bb6421670fedb7103055005c76f7961158
dylandeceulaer <dylan.deceulaer@student.howest.be>
2015-05-29 10:17:18 +0200

    hallo

---
+++ BusinessLogic/Repositories/ActiviteitRepository.cs
@@ -0,0 +1,27 @@
+﻿using Models.MVC_Models;
+using Models.OmgevingsBoek_Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace BusinessLogic.Repositories
+{
+    class ActiviteitRepository : GenericRepository<Activiteit>
+    {
+        public ActiviteitRepository(ApplicationDbContext context)
+            : base(context)
+        {
+
+        }
+
+        public override IEnumerable<Activiteit> All()
+        {
+            return this.context.Activiteiten.Include(i => i.Boeken);
+        }
+    }
+}
