66350214f65312d971d08226380ad3e3930f7038
Anthony Madhvani <anthony@madhvani.be>
2015-06-04 16:51:18 +0200

    Use distance calculator for POI list item template

--- Omgevingsboek/Content/js/filter.js
+++ Omgevingsboek/Content/js/filter.js
@@ -10,6 +10,25 @@ $('#searchPoi').bind('input', function () {
     filter($(this).val());
 });
 
+function distanceFrom(points) {
+    var lat1 = points.lat1;
+    var radianLat1 = lat1 * (Math.PI / 180);
+    var lng1 = points.lng1;
+    var radianLng1 = lng1 * (Math.PI / 180);
+    var lat2 = points.lat2;
+    var radianLat2 = lat2 * (Math.PI / 180);
+    var lng2 = points.lng2;
+    var radianLng2 = lng2 * (Math.PI / 180);
+    var earth_radius = 6371;
+    var diffLat = (radianLat1 - radianLat2);
+    var diffLng = (radianLng1 - radianLng2);
+    var sinLat = Math.sin(diffLat / 2);
+    var sinLng = Math.sin(diffLng / 2);
+    var a = Math.pow(sinLat, 2.0) + Math.cos(radianLat1) * Math.cos(radianLat2) * Math.pow(sinLng, 2.0);
+    var distance = earth_radius * 2 * Math.asin(Math.min(1, Math.sqrt(a)));
+    return distance.toFixed(3);
+}
+
 function filter(query) {
     var filtered = json;
 
@@ -76,11 +95,22 @@ function filter(query) {
         var tagSource = $("#poi-template").html();
         var tagTemplate = Handlebars.compile(tagSource);
 
+        console.log("TODO GEBRUIK ECHTE GEO DATA");
+
+        var distance = parseInt(distanceFrom({
+            // School locatie
+            'lat1': 51.20944,
+            'lng1': 3.22528,
+            // Poi
+            'lat2': 50.828056, 
+            'lng2': 3.265
+        }));
+
         var context = {
             title: home.poi.Naam,
             tags: home.poi.Tags.split(" ").map(function (el) { return '<span>' + el + '</span>' }).join(" "),
             image: home.Afbeelding,
-            distance: "?",
+            distance: distance,
             city: home.poi.Gemeente,
             numActivities: home.Activiteiten.length,
         };
