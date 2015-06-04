5bb8d17b7e249cdd39181e8b95308622a77a22bb
RoelKint <roelkint@gmail.com>
2015-06-04 11:01:51 +0200

    De Admin Push

--- Omgevingsboek/Controllers/AdminController.cs
+++ Omgevingsboek/Controllers/AdminController.cs
@@ -22,38 +22,25 @@ namespace Omgevingsboek.Controllers
         {
             return View();
         }
-        public ActionResult Activities()
-        {
-
-            return View(bs.GetActiviteitFirst50());
-        }
+        
         public ActionResult Activities(int? vanaf)
         {
             if (!vanaf.HasValue) return RedirectToAction("Activities");
             return View(bs.GetActiviteitNext50((int)vanaf));
         }
-        public ActionResult Gebruikers()
-        {
-            return View(bs.GetUserFirst50());
-        }
+       
         public ActionResult Gebruikers(int? vanaf)
         {
             if (!vanaf.HasValue) return RedirectToAction("Gebruikers");
             return View(bs.GetUserNext50((int)vanaf));
         }
-        public ActionResult Boeken()
-        {
-            return View(bs.GetBoekFirst50());
-        }
+        
         public ActionResult Boeken(int? vanaf)
         {
             if (!vanaf.HasValue) return RedirectToAction("Boeken");
             return View(bs.GetBoekNext50((int)vanaf));
         }
-        public ActionResult Pois()
-        {
-            return View(bs.GetPoiFirst50());
-        }
+        
         public ActionResult Pois(int? vanaf)
         {
             if (!vanaf.HasValue) return RedirectToAction("Pois");
