2913e20690522199070d2fc63877932a1f03084b
dylandeceulaer <dylan.deceulaer@student.howest.be>
2015-06-04 11:10:38 +0200

    admincontroller update

--- Omgevingsboek/Controllers/AdminController.cs
+++ Omgevingsboek/Controllers/AdminController.cs
@@ -22,41 +22,28 @@ namespace Omgevingsboek.Controllers
         {
             return View();
         }
-        public ActionResult Activities()
-        {
-
-            return View(bs.GetActiviteitFirst50());
-        }
+        
         public ActionResult Activities(int? vanaf)
         {
-            if (!vanaf.HasValue) return RedirectToAction("Activities");
+            if (!vanaf.HasValue) vanaf = 0;
             return View(bs.GetActiviteitNext50((int)vanaf));
         }
-        public ActionResult Gebruikers()
-        {
-            return View(bs.GetUserFirst50());
-        }
+        
         public ActionResult Gebruikers(int? vanaf)
         {
-            if (!vanaf.HasValue) return RedirectToAction("Gebruikers");
+            if (!vanaf.HasValue) vanaf = 0;
             return View(bs.GetUserNext50((int)vanaf));
         }
-        public ActionResult Boeken()
-        {
-            return View(bs.GetBoekFirst50());
-        }
+        
         public ActionResult Boeken(int? vanaf)
         {
-            if (!vanaf.HasValue) return RedirectToAction("Boeken");
+            if (!vanaf.HasValue) vanaf = 0;
             return View(bs.GetBoekNext50((int)vanaf));
         }
-        public ActionResult Pois()
-        {
-            return View(bs.GetPoiFirst50());
-        }
+        
         public ActionResult Pois(int? vanaf)
         {
-            if (!vanaf.HasValue) return RedirectToAction("Pois");
+            if (!vanaf.HasValue) vanaf = 0;
             return View(bs.GetPoiNext50((int)vanaf));
         }
     }
