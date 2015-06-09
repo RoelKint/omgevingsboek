11b0341efa785c5cb6b2797b9977f4358ddf41b9
dylandeceulaer <dylan.deceulaer@student.howest.be>
2015-06-09 10:00:44 +0200

    hallo

--- Omgevingsboek/Content/js/CooleDingenGebruiker.js
+++ Omgevingsboek/Content/js/CooleDingenGebruiker.js
@@ -2,6 +2,7 @@
     $('#profPic').hover(function () {
         $(this).fadeTo("slow", 0.5, function () {
             // Animation complete.
+            //amai zo cool zeg, amai
         });
     });
 });
