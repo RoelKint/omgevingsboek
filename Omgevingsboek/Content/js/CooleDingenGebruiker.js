5b3e6c9b27eeccb39f081511e81629e18457cf94
RoelKint <roelkint@gmail.com>
2015-06-10 15:58:06 +0200

    afwerking commit Roel 10/06 3.12.2

--- Omgevingsboek/Content/js/CooleDingenGebruiker.js
+++ Omgevingsboek/Content/js/CooleDingenGebruiker.js
@@ -19,60 +19,81 @@
 
     }
 
-  //function progressHandlingFunction(e) {
-  //    if (e.lengthComputable) {
-  //        $('progress').attr({ value: e.loaded, max: e.total });
-  //    }
-  //}
-  //
-  //function uploadNew() {
-  //    var formData = new FormData($('form')[0]);
-  //    $.ajax({
-  //        url: 'home/UpdateAfbeelding',  //Server script to process data
-  //        type: 'POST',
-  //        xhr: function () {  // Custom XMLHttpRequest
-  //            var myXhr = $.ajaxSettings.xhr();
-  //            if (myXhr.upload) { // Check if upload property exists
-  //                myXhr.upload.addEventListener('progress', progressHandlingFunction, false); // For handling the progress of the upload
-  //            }
-  //            return myXhr;
-  //        },
-  //        //Ajax events
-  //        beforeSend: beforeSendHandler,
-  //        success: completeHandler,
-  //        error: errorHandler,
-  //        // Form data
-  //        data: formData,
-  //        //Options to tell jQuery not to process data or worry about content-type.
-  //        cache: false,
-  //        contentType: false,
-  //        processData: false
-  //    });
-  //            $('progress').attr({ value: e.loaded, max: e.total });
-  //
-  //    }
-  //
-  //
-  //
-  //
-  //
-  //$("input:file").change(function () {
-  //    var file = this.files[0];
-  //    var name = file.name;
-  //    var size = file.size;
-  //    var type = file.type;
-  //
-  //    console.log(file);
-  //    console.log(name);
-  //    console.log(size);
-  //    //var fileName = $(this).val();
-  //    //console.log(fileName);
-  //    //$.post
-  //    console.log("jup");
-  //    uploadNew();
-  //    //$(".filename").html(fileName);
-  //});
-  //
+    function progressHandlingFunction(e) {
+        if (e.lengthComputable) {
+            $('progress').attr({ value: e.loaded, max: e.total });
+        }
+    }
+   
+    function uploadNew(file) {
+        console.log("hey");
+
+        var formData = new FormData();
+        
+        formData.append("Afbeelding", file);
+        $.ajax({
+            type: "POST",
+            url: "../Home/UpdateAfbeelding",
+            data: formData,
+            dataType: 'json',//change to your own, else read my note above on enabling the JsonValueProviderFactory in MVC
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                //BTW, data is one of the worst names you can make for a variable
+                //handleSuccessFunctionHERE(data);
+                console.log(':t');
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                //do your own thing
+                console.log("fail");
+            }
+        });
+       // $.ajax({
+       //     url: '../Home/UpdateAfbeelding',  //Server script to process data
+       //     type: 'POST',
+       //   // xhr: function () {  // Custom XMLHttpRequest
+       //   //     var myXhr = $.ajaxSettings.xhr();
+       //   //     if (myXhr.upload) { // Check if upload property exists
+       //   //         myXhr.upload.addEventListener('progress', progressHandlingFunction, false); // For handling the progress of the upload
+       //   //     }
+       //   //     return myXhr;
+       //   // },
+       //     //Ajax events
+       //     success: completeHandler,
+       //     // Form data
+       //     data: formData,
+       //     //Options to tell jQuery not to process data or worry about content-type.
+       //     cache: false,
+       //     contentType: false,
+       //     processData: false
+       // });
+                //$('progress').attr({ value: e.loaded, max: e.total });
+
+        }
+    
+    function completeHandler() {
+
+    }
+    
+
+
+    $("input:file").change(function () {
+        var file = this.files[0];
+        var name = file.name;
+        var size = file.size;
+        var type = file.type;
+
+        console.log(file);
+        console.log(name);
+        console.log(size);
+        //var fileName = $(this).val();
+        //console.log(fileName);
+        //$.post
+        console.log("jup");
+        uploadNew(file);
+        //$(".filename").html(fileName);
+    });
+
 
 
     console.log(parInfo);
---
+++ Omgevingsboek/Content/js/gebruikerpartialscript.js
@@ -0,0 +1,98 @@
+﻿function defer(method) {
+    if (window.jQuery) {
+        if (jQuery.ui)
+            method();
+    } else
+        setTimeout(function () { defer(method) }, 50);
+}
+
+$().ready(function () {
+    $('button[type=submit]').on('click', function () {
+        if ($('#addPoi').valid()) {
+            var $btn = $(this).button('loading')
+        }
+    })
+
+    $(".sidebar input:file").change(function () {
+        var fileName = $(this).val();
+        console.log(fileName);
+        $(".sidebar .fileUpload").toggleClass("upload-ok");
+        //$(".filename").html(fileName);
+    });
+
+    Array.prototype.getUnique = function () {
+        var u = {}, a = [];
+        for (var i = 0, l = this.length; i < l; ++i) {
+            if (u.hasOwnProperty(this[i])) {
+                continue;
+            }
+            a.push(this[i]);
+            u[this[i]] = 1;
+        }
+        return a;
+    }
+
+  
+
+    $("#amount").val("" + $("#slider-range").slider("values", 0) +
+            " - " + $("#slider-range").slider("values", 1));
+
+    $("#minAge").text("5");
+    $("#maxAge").text("10");
+    $("#minAgeHidden").val("5");
+    $("#maxAgeHidden").val("10");
+
+    $('#locationPicker').popover({
+        html: true,
+        content: function () {
+            return $('#location_picker_wrapper').html();
+        }
+    });
+    $('#locationPicker').on('shown.bs.popover', function () {
+        // set what happens when user clicks on the button
+        console.log("shown.bs.popover");
+        //$('.popover #us2').locationpicker('autosize');
+        $('.popover #us2').locationpicker({
+            location: { latitude: 50.8028051, longitude: 3.279785 },
+            radius: 0,
+            inputBinding: {
+                latitudeInput: $('.popover #us2-lat'),
+                longitudeInput: $('.popover #us2-lon'),
+                locationNameInput: $('.popover #us2-address')
+            },
+            enableAutocomplete: true,
+            onchanged: function (currentLocation, radius, isMarkerDropped) {
+                var addressComponents = $(this).locationpicker('map').location.addressComponents;
+                $('#inputAdres').val(addressComponents.addressLine1 + ", " + addressComponents.postalCode + " " + addressComponents.city + ", " + addressComponents.country);
+                $('#straatHidden').val(addressComponents.streetName);
+                $('#gemeenteHidden').val(addressComponents.city);
+                $('#postcodeHidden').val(addressComponents.postalCode);
+                $('#nummerHidden').val(addressComponents.streetNumber);
+                $('#latitudeHidden').val((currentLocation.latitude));
+                $('#latitudeHidden').val($('#latitudeHidden').val().replace(".", ","));
+                $('#longitudeHidden').val(currentLocation.longitude);
+                $('#longitudeHidden').val($('#longitudeHidden').val().replace(".", ","));
+
+            }
+        });
+    });
+
+    $("#expand").click(function () {
+        $(".sidebar > form").toggleClass("form-exp");
+        $(".sidebar .poi").toggleClass("form-exp");
+        $(".expandable").toggleClass("expandable-exp");
+        $(".expandable > #expand").toggleClass("expand-exp");
+        $(".content").toggleClass("content-exp");
+    });
+
+    $("#addPoi").click(function () {
+        $(".sidebar > form").toggleClass("hidden");
+        $(".sidebar .poi").toggleClass("hidden");
+    });
+
+    $("#viewPoi").click(function () {
+        $(".sidebar > form").toggleClass("hidden")
+        $(".sidebar .poi").toggleClass("hidden");
+    });
+
+});
