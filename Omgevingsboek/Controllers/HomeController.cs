459d9da7c044928f6c4907b57ecb0f0f51831575
dylandeceulaer <dylan.deceulaer@student.howest.be>
2015-06-15 15:35:47 +0200

    gfd

--- Omgevingsboek/Controllers/HomeController.cs
+++ Omgevingsboek/Controllers/HomeController.cs
@@ -188,6 +188,8 @@ namespace Omgevingsboek.Controllers
         {
             String fotoId;
             PhotoInfo fotoInfo;
+            poi.Prijs = 0;
+
             if (!ModelState.IsValid) return RedirectToAction("Index");
             
             String[] tags = TagsString.Split(',');
@@ -215,7 +217,9 @@ namespace Omgevingsboek.Controllers
                 Prijs = poi.Prijs,
                 Straat = poi.Straat,
                 Telefoon = poi.Telefoon,
-                Tags = tagList
+                Tags = tagList,
+                Latitude = poi.Latitude,
+                Longitude = poi.Longitude
             };
 
             if (AfbeeldingFile != null)
