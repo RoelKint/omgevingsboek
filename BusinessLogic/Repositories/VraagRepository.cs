5060d4470fa48fad1cddbefca5714a740fe91e42
dylandeceulaer <dylan.deceulaer@student.howest.be>
2015-06-17 21:59:25 +0200

    helpdesk

---
+++ BusinessLogic/Repositories/VraagRepository.cs
@@ -0,0 +1,45 @@
+﻿using Models;
+using Models.MVC_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Repositories
+{
+    class VraagRepository : GenericRepository<Vraag>
+    {
+        public VraagRepository(ApplicationDbContext context)
+            : base(context)
+        {
+            context.Configuration.LazyLoadingEnabled = false;
+        }
+        public VraagRepository()
+            : base(new ApplicationDbContext())
+        {
+            context.Configuration.LazyLoadingEnabled = false;
+        }
+
+        public List<Vraag> GetVragen()
+        {
+            return context.Vragen.Where(v => !v.IsDeleted).ToList();
+        }
+        public override Vraag Insert(Vraag entity)
+        {
+            return base.Insert(entity);
+        }
+        public void Opgelost(int VraagId)
+        {
+            Vraag vraag = GetByID(VraagId);
+            vraag.IsDeleted = true;
+            Update(vraag);
+            context.SaveChanges();
+        }
+        public override Vraag GetByID(object id)
+        {
+            return base.GetByID(id);
+        }
+
+    }
+}
---
+++ Omgevingsboek/Controllers/HelpDeskController.cs
@@ -0,0 +1,29 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Omgevingsboek.Controllers
+{
+    public class HelpDeskController : Controller
+    {
+        // GET: HelpDesk
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Authorize]
+        public ActionResult PostVraag (Vraag vraag)
+        {
+            return View();
+        }
+        [Authorize(Roles = "Administrator,SuperAdministrator")]        
+        public ActionResult IndexAdmin()
+        {
+            return View();
+        }
+    }
+}
