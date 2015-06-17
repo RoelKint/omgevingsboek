9f9c6a4962a0a96993d6b6c2c8990bd54e18b4ee
RoelKint <roelkint@gmail.com>
2015-06-17 03:06:57 +0200

    nog eens

--- BusinessLogic/Repositories/IRouteRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace BusinessLogic.Repositories
-{
-    public interface IRouteRepository
-    {
-        System.Collections.Generic.List<Models.OmgevingsBoek_Models.Route> getRoutesByBoek(int boekId);
-        Models.OmgevingsBoek_Models.Route Insert(Models.OmgevingsBoek_Models.Route entity);
-    }
-}
