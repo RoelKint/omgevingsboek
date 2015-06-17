c33befb26a2645437aebd5f8c6e6aadd91c58afc
Anthony Madhvani <anthony@madhvani.be>
2015-06-17 10:05:53 +0200

    sharing

--- Omgevingsboek/Content/js/shareItem.js
+++ Omgevingsboek/Content/js/shareItem.js
@@ -1,24 +1,34 @@
 ﻿//#share_item_wrapper
 $(function () {
-    $(".shareItem").popover({
-        html: true,
-        content: function () {
-            return $('#share_item_wrapper').html();
-        }
-    });
-    $('.shareItem').on('shown.bs.popover', function () {
-        console.log("shown.bs.popover");
-        $.get("/home/sharelist?Id=1&Type=boek", function (data) {
-            var people = $.parseJSON(data);
+    $.get("/home/sharelist?Id=1&Type=boek", function (data) {
+        console.log("Sharelist loaded succesfully");
+        var people = $.parseJSON(data);
+        $(".shareItem").popover({
+            html: true,
+            content: function () {
+                return $('#share_item_wrapper').html();
+            }
+        });
 
-            $("#share_item_wrapper .people")[0].innerHTML = "";
-            var container = $("#share_item_wrapper .people")[0];
-            people.forEach(function (person) {
-                var s = '<li>' + person.Naam + '</li>';
-                var el = document.createElement('div');
-                el.innerHTML = s;
-                container.appendChild(el)
-            })
+        $("#share_item_wrapper .people")[0].innerHTML = "";
+        var container = $("#share_item_wrapper .people")[0];
+        people.forEach(function (person) {
+            console.log(person.Naam);
+            //var s = '<option>' + person.Naam + '</option>';
+            var label = document.createElement('label');
+            label.innerHTML = person.Naam;
+            label.setAttribute("for", person.Username);
+
+            var el = document.createElement('input');
+            el.setAttribute("type", "checkbox");
+            el.setAttribute("name", "Username");
+            el.setAttribute("value", person.Username);
+            container.appendChild(label)
+            container.appendChild(el)
+        })
+
+        $('.shareItem').on('shown.bs.popover', function () {
+            console.log("shown.bs.popover");
         });
     });
-})
+});
